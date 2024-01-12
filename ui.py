72bacf63e16e06ee2012bdd2fe3e20171f4d2e2d
vill4lobos <vill4lobos@protonmail.com>
2024-01-11 23:36:02 -0300

    Cycle album list on y axis
    create_albums_list() created to cycle the list, but needs more work on it

--- ui.py
+++ ui.py
@@ -21,8 +21,11 @@ class UI(object):
         self.x_center = self.x_limit // 2
         self.positionx = 0
         self.positiony = 0
+        self.old_positiony = 0
         self.genres_height = 0
         self.albums_height = 2
+
+        self.last_lst = Get.album_list
 
     """
     Returns a list of tuples with all elements and their lengths that can fit
@@ -69,6 +72,28 @@ class UI(object):
 
         return lst_genres
 
+    def create_albums_list(self, lst):
+        limit = self.y_limit - self.albums_height - 1
+        test = self.positiony - self.old_positiony + 1
+
+        if test > limit:
+            self.old_positiony = self.positiony
+
+        if self.old_positiony < 0 and self.positiony < 0:
+            self.last_lst = lst[self.old_positiony - 1:] + lst[:self.old_positiony]
+
+        elif self.positiony < 0:
+            self.last_lst = lst[self.positiony:] + lst[:self.positiony]
+
+        elif self.positiony < self.old_positiony:
+            self.last_lst
+
+        elif self.positiony > limit:
+            self.old_positiony = self.positiony
+            self.last_lst = lst[self.positiony - limit:] + lst[:self.positiony - limit]
+
+        return self.last_lst
+
     def max_albums(self, lst, max):
         stop = 0
 
@@ -95,17 +120,59 @@ class UI(object):
                                curses.A_ITALIC)
 
     def display_albums(self):
-        max = self.max_albums(Get.album_list, self.y_limit)
+        lst_albums = self.create_albums_list(Get.album_list)
+        self.screen.addstr(0, 0, str(self.positiony))
+        self.screen.addstr(1, 0, str(self.old_positiony))
+        self.screen.addstr(2, 0, str(self.positiony - self.old_positiony + 1))
+        self.screen.addstr(3, 0, str(self.y_limit))
+        self.screen.addstr(4, 0, str(self.albums_height))
+        k = False
+        test = self.positiony - self.old_positiony + 1
 
-        for i, item in enumerate(max):
-            self.screen.addstr(1 + i, 20, item, curses.A_BLINK)
+        for i, item in enumerate(lst_albums):
+            if i == self.y_limit - self.albums_height:
+                break
+
+            if not k and \
+               ((i == self.positiony and (self.positiony >= 0
+                        and self.old_positiony >= 0)) or
+               (i == ((self.y_limit - self.albums_height) - 
+                      (self.old_positiony - self.positiony)) and
+                 self.old_positiony > self.positiony > 0) or
+                
+                (i == test and self.old_positiony < 0) or
+                #i == (self.old_positiony + self.old_positiony < 0
+                #(i == )
+                
+               (self.positiony < 0 and i == 0 and test < 0)):
+
+                self.screen.addstr(self.albums_height + i,
+                                   self.center_str(item), item,
+                                   curses.A_BLINK | curses.A_REVERSE)
+                k = True
+                continue
+
+            self.screen.addstr(self.albums_height + i,
+                               self.center_str(item), item, curses.A_BOLD)
+
+    def navigatex(self, b):
+        self.positionx += 1 if b else -1
+        self.display_ui()
 
     def navigatey(self, b):
-        self.positionx += 1 if b else -1
+        if not b and self.positiony < 0 and self.positiony < self.old_positiony:
+            self.old_positiony = self.positiony
+        #elif b and self.positiony > 0 
+        self.positiony += 1 if b else -1
+        
+        #if self.positiony >= 0:
+        #    self.old_positiony = 0
+        
         self.display_ui()
 
     def display_ui(self):
         self.screen.refresh()
+        self.screen.clear()
         self.display_genres()
         self.display_albums()
 
@@ -115,9 +182,10 @@ class Get:
     genre_list = [str(x) for x in range(100, 201)]
     #album_list = [list(string.ascii_lowercase)[rd.randint(0, 26)]
     #              for x in range(0, rd.randint(15, 30))]
-    album_list = [''.join([list(string.ascii_lowercase)[rd.randint(0, 25)]
+    album_list = [str(i) + "   " + x for i, x in enumerate(
+                  [''.join([list(string.ascii_lowercase)[rd.randint(0, 25)]
                   for x in range(0, rd.randint(15, 30))])
-                  for x in range(30)]
+                  for x in range(30)])]
 
     def __init__(self):
         pass
