38cff9e3ba8396fac70c21743983cc46cd781a67
vill4lobos <vill4lobos@protonmail.com>
2024-01-30 13:45:47 -0300

    Separate View from UI, create a few methods
    Created Get() to handle requests to the db

--- api.py
+++ api.py
@@ -1,26 +1,30 @@
 from flask import Flask, redirect, url_for
-from database import Database
+from database import Get
+#from livereload import Server
 
 app = Flask(__name__)
 app.config["DEBUG"]
 
-db = Database()
+db = Get()
 
 
 @app.route("/")
 def hello():
-    db.save()
     return "'Sup!"
 
 
 @app.route("/genres", methods=["GET"])
 def genres():
-    return db.get()
+    return db.default_get_albums()
+
+
+@app.route("/all", methods=["GET"])
+def get_all():
+    return db.get_all_albums()
 
 
 @app.route("/save", methods=["GET"])
 def save():
-    db.save()
     return redirect(url_for('genres'))
 
 
--- database.py
+++ database.py
@@ -16,25 +16,68 @@ class Database:
         with shelve.open(self.DB_PATH) as db:
             if not list(db.keys()):
                 db["genres"] = {}
+            if 'artists' not in list(db.keys()):
+                db["artists"] = {}
 
-    def save(self):
+    def save_albums(self, dict, artist):
+        with shelve.open(self.DB_PATH, writeback=True) as db:
+            for item in dict:
+                if item[0] in db["genres"].keys():
+                    if item[1] in db['genres'][item[0]]:
+                        continue
+                    db["genres"][item[0]].append(item[1])
+                else:
+                    if item[0] is None:
+                        item[0] = 'undefined'
+                    db["genres"].update({item[0]: [item[1]]})
+
+            db["artists"].update({'artists': artist})
+
+    def get_all(self):
+        with shelve.open(self.DB_PATH) as db:
+            return db["genres"]
+
+    def get_artist(self, artist):
+        with shelve.open(self.DB_PATH) as db:
+            return True if artist in db['artists'] else False
+
+
+class Get:
+
+    def __init__(self):
+
+        self.default_artist_lists = \
+            ['truckfighters', 'kyuss', 'sleep', 'damad', 'corrupted']
+
+        self.db = Database()
+
+        # TODO: populate db if 50 entries or less
+
+    def get_albums(self, artist):
+
+        artist = ''.join(filter(str.isalpha, artist))
+        if self.db.get_artist(artist):
+            return "Already exists"
+
         r = requests.get(
             "http://musicbrainz.org/ws/2/release-group/" +
-            "?query=artist:%22truckfighters%22&fmt=json").json()
+            "?query=artist:%22{}%22&fmt=json".format(artist)).json()
 
         fltr = jmespath.search(
             '"release-groups"[?"primary-type"==`Album`].\
             [tags[0].name, title]', r)
 
-        with shelve.open(self.DB_PATH, writeback=True) as db:
-            for item in fltr:
-                if item[0] in db["genres"].keys():
-                    if item[1] in db['genres'][item[0]]:
-                        continue
-                    db['genres'][item[0]].append(item[1])
-                else:
-                    db['genres'].update({item[0]: [item[1]]})
+        self.db.save_albums(fltr, artist)
 
-    def get(self):
-        with shelve.open(self.DB_PATH) as db:
-            return list(db.items())
+        return fltr
+
+    def default_get_albums(self):
+        rtn_lst = []
+
+        for item in self.default_artist_lists:
+            rtn_lst.append(self.get_albums(item))
+
+        return rtn_lst
+
+    def get_all_albums(self):
+        return self.db.get_all()
