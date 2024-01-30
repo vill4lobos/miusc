279bab4095d3ceb4a557ad89e4f41198246f987d
vill4lobos <vill4lobos@protonmail.com>
2024-01-29 21:05:24 -0300

    Improve x axis moves, changing to dequeue too
    Using dequeue to cycle genres and changed how display() works

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@ import curses
 import string
 import random as rd
 from collections import deque
-# from functools import reduce
+from itertools import islice
 
 
 def debug():
@@ -20,14 +20,14 @@ class UI(object):
         self.screen = screen
         self.y_limit, self.x_limit = screen.getmaxyx()
         self.x_center = self.x_limit // 2
-        self.positionx = 0
         self.genres_height = 0
         self.albums_height = 2
         self.last_movement = None
         self.y_index = 0
         self.last_axis = ''
 
-        self.dq = deque(Get.get_albums())
+        self.dq_album = deque(Get.get_albums())
+        self.dq_genre = deque(Get.genre_list)
 
     """
     Returns a list of tuples with all elements and their lengths that can fit
@@ -35,77 +35,67 @@ class UI(object):
     """
     # TODO: fix list out of range when more than 90 movements in the same
     # direction
-    def create_genres_list(self, lst):
-        center = self.center_str(lst[self.positionx])
-        left = center
-        right = center  # + len(lst[0])
-        left_go = self.positionx
+    def move_genres_list(self, move=False):
 
-        lst_genres = [(center, lst[self.positionx])]
-
-        i, j, stop, b = 0, self.positionx, 0, False
-        while i < len(lst) - 1:
-            b = not b
-
-            if b:
-                right = right + len(lst[j]) + self.GENRE_SEPARATE
-
-                if right >= self.x_limit:
-                    break
-
-                lst_genres.append((right, lst[j]))
-                stop += len(lst[j]) + self.GENRE_SEPARATE
-                j += 1
-
+        if move:
+            if self.last_movement:
+                self.dq_genre.rotate(-1)
             else:
-                left_go -= 1
-                left = left - len(lst[left_go]) - self.GENRE_SEPARATE
+                self.dq_genre.rotate(1)
 
-                if left <= 0:
-                    break
+        count = 0
+        i, j = 0, 0
+        dq_middle = int(len(self.dq_genre) / 2)
 
-                lst_genres.append((left, lst[left_go]))
-                stop += len(lst[left_go]) + self.GENRE_SEPARATE
+        for i, j in enumerate(range(dq_middle, len(self.dq_genre)), 1):
+            count += self.GENRE_SEPARATE * 2
 
-            if stop >= self.x_limit:
+            if (count + len(self.dq_genre[dq_middle - i]) +
+               len(self.dq_genre[j])) < self.x_limit:
+                count += len(self.dq_genre[dq_middle - i]) + \
+                    len(self.dq_genre[j])
+            else:
                 break
 
-            i += 1
-
-        return lst_genres
+        return deque(islice(self.dq_genre, dq_middle - i, j))
 
     def move_albums_list(self):
         limit = self.y_limit - self.albums_height - 1
 
-        if self.last_movement is True:
+        if self.last_movement:
             if self.y_index == limit:
-                self.dq.rotate(-1)
+                self.dq_album.rotate(-1)
             else:
                 self.y_index += 1
 
-        elif self.last_movement is False:
+        elif not self.last_movement:
             if self.y_index == 0:
-                self.dq.rotate(1)
+                self.dq_album.rotate(1)
             elif self.y_index != 0:
                 self.y_index -= 1
 
     def center_str(self, str):
         return self.x_center - (len(str) // 2)
 
-    def display_genres(self):
-        lst_genres = self.create_genres_list(Get.genre_list)
-        self.screen.addstr(0, lst_genres[0][0], lst_genres[0][1],
-                           curses.A_REVERSE)
-        lst_genres.pop(0)
+    def display_genres(self, move=False):
+        lst_genres = self.move_genres_list(move)
 
-        for i, item in lst_genres:
-            self.screen.addstr(0, i, item,
-                               curses.A_ITALIC)
+        # TODO: padding around the dq_genres
+        len_total = 0  # max(self.x_limit - sum(map(len, lst_genres)), 0) // 2
+        for i, item in enumerate(lst_genres):
+            if i == len(lst_genres) // 2 + 1:
+                self.screen.addstr(0, len_total, item,
+                                   curses.A_REVERSE)
+            else:
+                self.screen.addstr(0, len_total, item,
+                                   curses.A_ITALIC)
+
+            len_total += len(item) + 2
 
     def display_albums(self, move=False):
         if move:
             self.move_albums_list()
-        lst_albums = self.dq
+        lst_albums = self.dq_album
 
         for i, item in enumerate(lst_albums):
             if i == self.y_limit - self.albums_height:
@@ -115,14 +105,13 @@ class UI(object):
                 self.screen.addstr(self.albums_height + i,
                                    self.center_str(item), item,
                                    curses.A_BLINK | curses.A_REVERSE)
-                continue
-
-            self.screen.addstr(self.albums_height + i,
-                               self.center_str(item), item, curses.A_BOLD)
+            else:
+                self.screen.addstr(self.albums_height + i,
+                                   self.center_str(item), item, curses.A_BOLD)
 
     def x_navigate(self, b):
         self.last_axis = 'x'
-        self.positionx += 1 if b else -1
+        self.last_movement = True if b else False
         self.display_ui()
 
     def y_navigate(self, b):
@@ -136,18 +125,18 @@ class UI(object):
 
         if self.last_axis == 'y':
             self.display_albums(True)
+            self.display_genres()
 
         elif self.last_axis == 'x':
-            self.dq = deque(Get.get_albums())
+            self.dq_album = deque(Get.get_albums())
             self.y_index = 0
             self.display_albums()
-
-        self.display_genres()
+            self.display_genres(True)
 
 
 class Get:
 
-    genre_list = [str(x) for x in range(100, 201)]
+    genre_list = [str(x) for x in range(1000, 1050)]
     # album_list = [list(string.ascii_lowercase)[rd.randint(0, 26)]
     #               for x in range(0, rd.randint(15, 30))]
 
@@ -157,9 +146,9 @@ class Get:
     @staticmethod
     def get_albums():
         return [str(i) + "   " + x for i, x in enumerate(
-                  [''.join([list(string.ascii_lowercase)[rd.randint(0, 25)]
-                  for x in range(0, rd.randint(15, 30))])
-                  for x in range(30)])]
+                [''.join([list(string.ascii_lowercase)[rd.randint(0, 25)]
+                 for x in range(0, rd.randint(15, 30))])
+                 for x in range(30)])]
 
 
 def main(screen):
